1f1cc9630085b99a37b8a6caf0fad8e41305974a
Piotr Makowski <piotr.makowski@domeny.pl>
2016-03-16 18:21:10 +0100

    [PMakowski] #2.5.1.x-dev: Adding option to define editor path and open files in that during interactive session

--- src/Config.php
+++ src/Config.php
@@ -115,6 +115,7 @@ class Config
                          'tabWidth'        => null,
                          'encoding'        => null,
                          'extensions'      => null,
+                         'editorPath'      => null,
                          'sniffs'          => null,
                          'exclude'         => null,
                          'ignored'         => null,
@@ -472,6 +473,7 @@ class Config
         $this->reportFile      = null;
         $this->generator       = null;
         $this->filter          = null;
+        $this->editorPath      = null;
         $this->bootstrap       = array();
         $this->basepath        = null;
         $this->reports         = array('full' => null);
@@ -488,6 +490,11 @@ class Config
         $standard = self::getConfigData('default_standard');
         if ($standard !== null) {
             $this->standards = explode(',', $standard);
+        }
+
+        $editorPath = self::getConfigData('editorPath');
+        if ($editorPath !== null) {
+            $this->editorPath = $editorPath;
         }
 
         $reportFormat = self::getConfigData('report_format');
@@ -821,6 +828,9 @@ class Config
 
                 $this->sniffs = $sniffs;
                 $this->overriddenDefaults['sniffs'] = true;
+            } else if (substr($arg, 0, 12) === 'editor-path=') {
+                $this->editorPath = substr($arg, 12);
+                $this->overriddenDefaults['editorPath'] = true;
             } else if (substr($arg, 0, 8) === 'exclude=') {
                 if (isset($this->overriddenDefaults['exclude']) === true) {
                     break;
@@ -1333,7 +1343,7 @@ class Config
         echo '  [--report-width=<reportWidth>] [--basepath=<basepath>] [--bootstrap=<bootstrap>]'.PHP_EOL;
         echo '  [--severity=<severity>] [--error-severity=<severity>] [--warning-severity=<severity>]'.PHP_EOL;
         echo '  [--runtime-set key value] [--config-set key value] [--config-delete key] [--config-show]'.PHP_EOL;
-        echo '  [--standard=<standard>] [--sniffs=<sniffs>] [--exclude=<sniffs>]'.PHP_EOL;
+        echo '  [--standard=<standard>] [--sniffs=<sniffs>] [--exclude=<sniffs>] [--editor-path=<path>]'.PHP_EOL;
         echo '  [--encoding=<encoding>] [--parallel=<processes>] [--generator=<generator>]'.PHP_EOL;
         echo '  [--extensions=<extensions>] [--ignore=<patterns>] [--ignore-annotations]'.PHP_EOL;
         echo '  [--stdin-path=<stdinPath>] [--file-list=<fileList>] <file> - ...'.PHP_EOL;
