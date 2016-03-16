e2af79f8d1838209845e5eaf7cf28a1ef50ca4ae
Piotr Makowski <piotr.makowski@domeny.pl>
2016-03-16 20:16:46 +0100

    [PMakowski] #2.5.1.x-dev: again matching PHPCS coding standard

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -145,7 +145,7 @@ class PHP_CodeSniffer
      * @var array
      */
     private $_tokenListeners = array();
-    
+
     /**
      * The editor path / command
      *
@@ -404,6 +404,7 @@ class PHP_CodeSniffer
     public function getEditorPath()
     {
         return $this->editorPath;
+
     }//end getEditorPath()
 
 
@@ -417,6 +418,7 @@ class PHP_CodeSniffer
     public function setEditorPath($path)
     {
         $this->editorPath = $path;
+
     }//end setEditorPath()
 
 
@@ -428,6 +430,7 @@ class PHP_CodeSniffer
     public function isEditorPathSet()
     {
         return false === empty($this->editorPath);
+
     }//end isEditorPathSet()
 
 
@@ -1833,7 +1836,7 @@ class PHP_CodeSniffer
             $reportData  = $this->reporting->prepareFileReport($phpcsFile);
             $reportClass->generateFileReport($reportData, $phpcsFile, $cliValues['showSources'], $cliValues['reportWidth']);
 
-            if ($this->isEditorPathSet()) {
+            if ($this->isEditorPathSet() === true) {
                 echo '<ENTER> to recheck, [s] to skip, [o] to open in editor or [q] to quit : ';
             } else {
                 echo '<ENTER> to recheck, [s] to skip or [q] to quit : ';
@@ -1849,7 +1852,7 @@ class PHP_CodeSniffer
                 exit(0);
                 break;
             case 'o':
-                if ($this->isEditorPathSet()) {
+                if ($this->isEditorPathSet() === true) {
                     exec($this->getEditorPath().' '.$file);
                 }
                 break;
